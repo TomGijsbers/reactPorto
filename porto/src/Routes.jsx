68863755e576c0ede36507f89b5775376b8ea45d
TomGijsbers <r0878924@student.thomasmore.be>
2025-04-04 16:21:48 +0200

    vercel solution

--- porto/src/Routes.jsx
+++ porto/src/Routes.jsx
@@ -1,17 +1,20 @@
-import { Routes, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./views/Home";
 import About from "./views/About";
 import Art from "./views/Art";
-import Game from "./views/game"; // Add this import
+import Game from "./views/Game"; // Make sure this is correctly cased
 
 const AppRoutes = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/art" element={<Art />} />
-      <Route path="/game" element={<Game />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/art" element={<Art />} />
+        <Route path="/game" element={<Game />} /> {/* Make sure this matches */}
+      </Routes>
+    </Router>
   );
 };
 
