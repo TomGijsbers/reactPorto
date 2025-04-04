08ce4759d7d81f8d19481721635c967fa0473723
TomGijsbers <r0878924@student.thomasmore.be>
2025-04-04 16:26:23 +0200

    Solution2

--- porto/src/App.jsx
+++ porto/src/App.jsx
@@ -1,19 +1,11 @@
 import AppRoutes from "./Routes";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-// Create a theme with both light and dark mode support
-const theme = createTheme({
-  palette: {
-    mode: 'light', // Default to light mode
-  },
-});
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
+    <div>
       <AppRoutes />
-    </ThemeProvider>
+    </div>
   );
 }
 
