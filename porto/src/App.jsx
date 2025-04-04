f65a44b4153767b7ce7de5cdd157d8aea284f0de
TomGijsbers <r0878924@student.thomasmore.be>
2025-04-04 16:30:43 +0200

    solut3

--- porto/src/App.jsx
+++ porto/src/App.jsx
@@ -1,11 +1,19 @@
 import AppRoutes from "./Routes";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+// Create a theme with both light and dark mode support
+const theme = createTheme({
+  palette: {
+    mode: 'light', // Default to light mode
+  },
+});
 
 function App() {
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <AppRoutes />
-    </div>
+    </ThemeProvider>
   );
 }
 
--- porto/src/Routes.jsx
+++ porto/src/Routes.jsx
@@ -1,20 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./views/Home";
 import About from "./views/About";
 import Art from "./views/Art";
-import Game from "./views/Game"; // Make sure this is correctly cased
+import Game from "./views/Game";
 
 const AppRoutes = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/art" element={<Art />} />
-        <Route path="/game" element={<Game />} /> {/* Make sure this matches */}
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/art" element={<Art />} />
+      <Route path="/game" element={<Game />} />
+    </Routes>
   );
 };
 
