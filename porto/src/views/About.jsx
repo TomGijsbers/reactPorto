d9827f61d16cf2115f3100c1c35b5ad44f853c97
TomGijsbers <r0878924@student.thomasmore.be>
2025-12-19 10:33:54 +0100

    About page styling

--- porto/src/views/About.jsx
+++ porto/src/views/About.jsx
@@ -1,15 +1,31 @@
-import Tabs from "../component/Tab"; // Zorg dat het pad klopt
-import NavBar from "../component/Nav"; // Zorg dat het pad klopt
+import React, { useState } from "react";
+import Tabs from "../component/Tab"; 
+import NavBar from "../component/Nav";
+import Footer from "../component/Footer";
+import "./css/Home.css"; // Import Home.css for the background styles
 
 
 const About = () => {
+    // State for dark mode
+    const [darkMode, setDarkMode] = useState(false);
+    
+    // Toggle dark mode
+    const toggleDarkMode = () => {
+        setDarkMode(!darkMode);
+    };
+
     return (
-        <>  
-        <NavBar />
-        <div style={{ textAlign: "center", marginTop: "50px" }}>
-            <Tabs />
+        <div className={darkMode ? "dark-mode" : ""}>  
+            <NavBar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+            
+            <div className="home-container" style={{ paddingTop: "2rem" }}>
+                <div style={{ textAlign: "center" }}>
+                    <Tabs />
+                </div>
+            </div>
+            
+            <Footer />
         </div>
-        </>
     );
 };
 
